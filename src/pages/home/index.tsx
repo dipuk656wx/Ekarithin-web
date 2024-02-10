ff028eeb41c4b82ffb92eb5aa4045514959692e7
SHUBHAM SINGH <subhamsingh108932@gmail.com>
2024-02-10 23:29:55 +0530

    update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,22 +4,22 @@ import axios from "axios";
 import Header from "../../components/header";
 import EventCard from "../../components/EventCard/EventCard";
 
-
-
-
 const Home = () => {
-    return (
-        <div
-            className="fixed inset-0 grid text-white"
-            style={{ gridTemplateRows: "auto 1fr auto" }}
-        >
-            < Header />
-            {/* <EventCard/> */}
-            <div className="w-[100vw] h-[100vh]" style={{background: "/images/backGround.svg"}}>
-
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className="fixed inset-0 grid text-white"
+      style={{ gridTemplateRows: "auto 1fr auto" }}
+    >
+      <Header />
+      {/* <EventCard/> */}
+      <div
+        className="w-[100vw] h-[100vh]"
+        style={{ background: "/images/backGround.svg" }}
+      >
+        <iframe src="/index.html" width="100%" height="100%" ></iframe>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
